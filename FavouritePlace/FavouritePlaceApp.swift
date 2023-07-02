fb504d8354b669f2915f6d73b8c07f44bafcedd0
Oluwatobi Oladipupo <oladipupo_tobi@yahoo.com>
2023-07-02 07:38:25 +0100

    Initial Commit

---
+++ FavouritePlace/FavouritePlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritePlaceApp.swift
+//  FavouritePlace
+//
+//  Created by Oladipupo Oluwatobi on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouritePlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
